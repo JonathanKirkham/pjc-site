a6843fcb563e59f4eb37597415a2762e5de7671b
JonathanKirkham <53611651+JonathanKirkham@users.noreply.github.com>
2020-05-20 10:25:11 +0200

    second commit

---
+++ public/js/script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    var clicked = 0;
+    $('.menu-btn').click(function(e){
+
+        var screen_height = $(window).height() - $('nav').height();
+
+        $(this).toggleClass('active');
+        $('body').toggleClass('menu-active');
+
+        if (clicked == 0) {
+            $('nav').height(screen_height);
+            clicked = 1;
+            return;
+        }
+
+        if (clicked == 1) {
+            $('nav').removeAttr('style');
+            clicked = 0;
+            return;
+        }
+
+    });
+
+});
